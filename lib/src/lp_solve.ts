e2f158d707e00d43db5db0c8d5cb7b7e97420566
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-04-09 11:33:48 +0200

    Adds unique identifier and fixes test

--- lib/src/lp_solve.ts
+++ lib/src/lp_solve.ts
@@ -4,12 +4,15 @@ import { OptimizationResult, parseGMPLOutput } from './parser';
 import process from 'node:child_process'
 import util from "node:util";
 
+import { v4 as uuidv4 } from 'uuid';
+
 const execPromise = util.promisify(process.exec);
 
 export async function executeGLPSol(input: string): Promise<OptimizationResult> {
   try {
-    const inputFileName = 'input.mod';
-    const outputFileName = 'output';
+    const uuid = uuidv4();
+    const inputFileName = 'input-' + uuid;
+    const outputFileName = 'output-' + uuid;
 
     // Write input content to input.mod file
     await fs.promises.writeFile(inputFileName, input);
@@ -20,7 +23,7 @@ export async function executeGLPSol(input: string): Promise<OptimizationResult> 
     // Read output file content
     const stdout = await fs.promises.readFile(outputFileName, 'utf-8');
 
-    // Remove input and output files
+    // // Remove input and output files
     await fs.promises.unlink(inputFileName);
     await fs.promises.unlink(outputFileName);
 
@@ -53,26 +56,13 @@ async function test() {
   if (res_string == JSON.stringify(baseline)) console.log("Equal to baseline");
   else console.log("Non equal to baseline")
 
-  // Failure test
-  input = fs.readFileSync("../test/model_fail.mod", { encoding: 'utf-8' });
-  res = await executeGLPSol(input)
-
-  baseline = parseGMPLOutput(
-    fs.readFileSync("../test/model_fail.output", { encoding: 'utf-8' })
-  );
-
-  res.details.problem = "override";
-  baseline.details.problem = "override";
-
-  res_string = JSON.stringify(res);
-
-  console.log("=== OUTPUT ===")
-  console.log(res_string);
-
-  fs.writeFileSync("res", JSON.stringify(res, null, 2), { encoding: 'utf-8' })
-
-  if (res_string == JSON.stringify(baseline)) console.log("Equal to baseline");
-  else console.log("Non equal to baseline")
+  try {
+    // Failure test
+    input = fs.readFileSync("../test/model_fail.mod", { encoding: 'utf-8' });
+    res = await executeGLPSol(input)
+  } catch (e) {
+    console.log("Failed as expected");
+  }
 }
 
 test();
